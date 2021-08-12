ec91a6fdbe36595c8807a70c98ac9f59bfa790a6
Michael Towson <MTowson@gmail.com>
2021-08-12 13:28:42 -0700

    file reorganization

--- server.js
+++ server.js
@@ -1,6 +1,14 @@
+// Dependencies
 const express = require("express");
 const mongoose = require('mongoose');
 const session = require('express-session');
-
-const app = express();
 const cors = require('cors')
+
+// initialize express
+const app = express();
+
+// Load config setting the .env path to /config/.env
+dotenv.config({ path: './config/.env' });
+
+// connect to database
+connectDb();
