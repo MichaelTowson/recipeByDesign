d34e9a6a4b579f17053ffcb5ce461ccc543224d1
Michael Towson <MTowson@gmail.com>
2021-08-18 17:59:11 -0700

    correct style sheet links

--- src/client_routes.js
+++ src/client_routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {res.render('Index')});
-router.get('/search', (req, res) => {res.render('Search')});
+router.get('/recipes', (req, res) => {res.render('Recipes')});
 router.get('/roulette', (req, res) => {res.render('Roulette')});
 router.get('/contact', (req, res) => {res.render('Contact')});
 router.get('/cocktails', (req, res) => {res.render('Cocktails')});
